ffa40a84fb5d124cb5f5b4d06a4ac590c49cf3c0
TUSHAR MISHRA <109910958+MISHRA-TUSHAR@users.noreply.github.com>
2024-01-18 11:05:23 +0530

    Add files via upload

--- प्रन्यAI/lib/screens/secondary/acts.dart
+++ प्रन्यAI/lib/screens/secondary/acts.dart
@@ -1,11 +1,82 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:pdf/widgets.dart' as pw;
+import 'package:share_plus/share_plus.dart';
 
 class ActSections extends StatelessWidget {
+  Future<List<int>> _generatePdf() async {
+    final pdf = pw.Document();
+
+    pdf.addPage(pw.Page(
+      build: (pw.Context context) {
+        return pw.Center(
+          child: pw.Text('Investigation Summary for Acts/Sections'),
+        );
+      },
+    ));
+
+    return pdf.save();
+  }
+
+  Future<void> _savePdf(BuildContext context) async {
+    final pdfBytes = await _generatePdf();
+
+    try {
+      // Get the document directory
+      final directory = await getApplicationDocumentsDirectory();
+
+      // Create a File instance using the document directory and the file name
+      final pdfFile = File('${directory.path}/acts_sections_summary.pdf');
+
+      // Write PDF data to file
+      await pdfFile.writeAsBytes(pdfBytes);
+
+      print('PDF saved successfully at: ${pdfFile.path}');
+
+      // Show a snackbar upon successful saving
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('PDF saved successfully at: ${pdfFile.path}'),
+          duration: Duration(seconds: 3),
+        ),
+      );
+
+      // Share the PDF file using share_plus
+      await Share.shareFiles([pdfFile.path], text: 'Investigation Summary PDF');
+    } catch (e) {
+      print('Error saving PDF: $e');
+      // Show a snackbar on error
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Error saving PDF: $e'),
+          duration: Duration(seconds: 3),
+        ),
+      );
+    }
+  }
+
+  Future<void> _shareText(BuildContext context) async {
+    final text = 'Investigation Summary for Acts/Sections\n\n'
+        'Based on the context provided in the statement, it appears that an excess payment of Rs. 13,28,219 was made for various public works projects in violation of rules. However, the message does not provide sufficient information to determine which specific Indian Penal Code (IPC) sections may apply. Here are some possible IPC sections that could be relevant based on the given context:\n\n'
+        '1. Criminal Breach of Trust by Public Servant (Section 403):\n'
+        'This section applies when a public servant, entrusted with property or money, dishonestly misappropriates or converts it to his own use, or intends to convert it to his own use, or allows any other person so to do, without lawful authority. In this context, if the Sarpanch and Gram Vikas Adhikari are found to have misappropriated funds intended for public works projects, this section may apply.\n\n'
+        '2. Criminal Conspiracy (Section 120B):\n'
+        'This section applies when two or more persons agree to commit an offense or to aid, abet, or procure its commission. If it is determined that the Sarpanch and Gram Vikas Adhikari conspired to withdraw government funds without executing the intended works, this section may apply.\n\n'
+        '3. Cheating (Section 415):\n'
+        'This section applies when a person by deceit, fraud, or misrepresentation, or by concealing the truth, induces another person to deliver any property or valuable security to him or fails to perform a promise or contract. If it is found that false statements were made or information was concealed in order to secure government funds for works that were never executed, this section may apply.\n\n'
+        '4. Forgery (Section 465):\n'
+        'This section applies when a person makes a false document or alters a genuine document with the intention of causing damage or injury to another person or property. If it is determined that false documents were created or existing documents were altered in order to secure government funds for non-existent works, this section may apply.';
+
+    // Share the text using share_plus
+    await Share.share(text, subject: 'Investigation Summary for Acts/Sections');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Summarize FIR'),
+        title: Text('Summarize FIR - Acts/Sections'),
       ),
       body: SingleChildScrollView(
         child: Padding(
@@ -21,7 +92,7 @@ class ActSections extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                    'Investigation Summary',
+                    'Investigation Summary for Acts/Sections',
                     style: TextStyle(
                       fontSize: 24.0,
                       fontWeight: FontWeight.bold,
@@ -50,6 +121,38 @@ class ActSections extends StatelessWidget {
                     '4. Forgery (Section 465):',
                     'This section applies when a person makes a false document or alters a genuine document with the intention of causing damage or injury to another person or property. If it is determined that false documents were created or existing documents were altered in order to secure government funds for non-existent works, this section may apply.',
                   ),
+                  SizedBox(height: 16.0),
+                  ElevatedButton(
+                    onPressed: () => _savePdf(context),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.orange,
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                    ),
+                    child: Text(
+                      'Save as PDF',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                  SizedBox(height: 16.0),
+                  ElevatedButton(
+                    onPressed: () => _shareText(context),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.green,
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                    ),
+                    child: Text(
+                      'Share Text',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
                 ],
               ),
             ),
@@ -77,4 +180,10 @@ class ActSections extends StatelessWidget {
       ],
     );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: ActSections(),
+  ));
 }
--- प्रन्यAI/lib/screens/secondary/lorem.dart
+++ प्रन्यAI/lib/screens/secondary/lorem.dart
@@ -1,6 +1,90 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:pdf/widgets.dart' as pw;
+import 'package:share_plus/share_plus.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LoremPage extends StatelessWidget {
+  Future<List<int>> _generatePdf() async {
+    final pdf = pw.Document();
+
+    pdf.addPage(pw.Page(
+      build: (pw.Context context) {
+        return pw.Center(
+          child: pw.Text('Investigation Summary'),
+        );
+      },
+    ));
+
+    return pdf.save();
+  }
+
+  Future<void> _savePdf(BuildContext context) async {
+    final pdfBytes = await _generatePdf();
+
+    try {
+      // Get the document directory
+      final directory = await getApplicationDocumentsDirectory();
+
+      // Create a File instance using the document directory and the file name
+      final pdfFile = File('${directory.path}/investigation_summary.pdf');
+
+      // Write PDF data to file
+      await pdfFile.writeAsBytes(pdfBytes);
+
+      print('PDF saved successfully at: ${pdfFile.path}');
+
+      // Show a snackbar upon successful saving
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('PDF saved successfully at: ${pdfFile.path}'),
+          duration: Duration(seconds: 3),
+        ),
+      );
+
+      // Save PDF file path in SharedPreferences
+      final prefs = await SharedPreferences.getInstance();
+      prefs.setString('pdfFilePath', pdfFile.path);
+    } catch (e) {
+      print('Error saving PDF: $e');
+      // Show a snackbar on error
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Error saving PDF: $e'),
+          duration: Duration(seconds: 3),
+        ),
+      );
+    }
+  }
+
+  Future<void> _shareSavedPdf(BuildContext context) async {
+    // Retrieve PDF file path from SharedPreferences
+    final prefs = await SharedPreferences.getInstance();
+    final pdfFilePath = prefs.getString('pdfFilePath');
+
+    if (pdfFilePath != null) {
+      // Share the PDF file using share_plus
+      await Share.shareFiles([pdfFilePath], text: 'Investigation Summary PDF');
+    } else {
+      // Show a snackbar if the PDF file path is not found
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('PDF file not found. Save the PDF first.'),
+          duration: Duration(seconds: 3),
+        ),
+      );
+    }
+  }
+
+  Future<void> _shareText(BuildContext context) async {
+    final text = 'Investigation Summary\n\n'
+        'An investigation was conducted into allegations of excess payments made during the tenure of Gram Panchayat Beranadi for various works, including pipeline laying and shed construction. The investigation found that signatures of a competent engineer were missing on some approvals and no measurement book was filled for one of the works. A preliminary inquiry was registered and verified, with necessary documents obtained. Some documents lacked required signatures or were incomplete.';
+
+    // Share the text using share_plus
+    await Share.share(text, subject: 'Investigation Summary');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,6 +117,54 @@ class LoremPage extends StatelessWidget {
                     'An investigation was conducted into allegations of excess payments made during the tenure of Gram Panchayat Beranadi for various works, including pipeline laying and shed construction. The investigation found that signatures of a competent engineer were missing on some approvals and no measurement book was filled for one of the works. A preliminary inquiry was registered and verified, with necessary documents obtained. Some documents lacked required signatures or were incomplete.',
                     style: TextStyle(fontSize: 16.0, color: Colors.grey[800]),
                   ),
+                  SizedBox(height: 16.0),
+                  ElevatedButton(
+                    onPressed: () => _savePdf(context),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.orange,
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                    ),
+                    child: Text(
+                      'Save as PDF',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                  SizedBox(height: 16.0),
+                  ElevatedButton(
+                    onPressed: () => _shareSavedPdf(context),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.blue,
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                    ),
+                    child: Text(
+                      'Share Saved PDF',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                  SizedBox(height: 16.0),
+                  ElevatedButton(
+                    onPressed: () => _shareText(context),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.green,
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                    ),
+                    child: Text(
+                      'Share Text',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
                 ],
               ),
             ),
@@ -41,4 +173,10 @@ class LoremPage extends StatelessWidget {
       ),
     );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: LoremPage(),
+  ));
 }
